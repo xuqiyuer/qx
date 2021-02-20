0bcb60ce83b0fb9c9ad449006cb6d13637e42330
qiyuer <645113226@qq.com>
2021-02-20 23:53:40 +0800

    Update jd_speed_sign.js

--- jd_speed_sign.js
+++ jd_speed_sign.js
@@ -10,7 +10,7 @@
 ============Quantumultx===============
 [task_local]
 #京东极速版
-0 7 * * * https://gitee.com/lxk0301/jd_scripts/raw/master/jd_speed_sign.js, tag=京东极速版, img-url=https://raw.githubusercontent.com/Orz-3/task/master/jd.png, enabled=true
+0 7 * * * https://raw.githubusercontent.com/xuqiyuer/qx/main/jd_speed_sign.js, tag=京东极速版, img-url=https://raw.githubusercontent.com/Orz-3/task/master/jd.png, enabled=true
 
 ================Loon==============
 [Script]
